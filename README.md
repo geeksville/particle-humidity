7f17bc7aa047999bf455b67f08fa7b531fdfca09
Kevin Hester <kevinh@geeksville.com>
2019-01-03 19:45:56 -0800

    Add IFTTT trigger for humidity too high

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # A small humidity sensor for my garage
 
-NOTE: This project is not yet complete.  This readme currently is a raw set of notes to myself...
+You can see the output of this sensor here: https://io.adafruit.com/punkgeek/dashboards/garage
 
 ## Steps to build/install
 
@@ -19,28 +19,15 @@ Steps required:
 
 * The LED on the photon has some helpful [blink codes](https://docs.particle.io/tutorials/device-os/led/photon/)
 
-## Notes yet to categorize
+## Implementation notes
 
-use particle board to ifttt
+* use particle board to ifttt
 
-sensor: https://learn.sparkfun.com/tutorials/si7021-humidity-and-temperature-sensor-hookup-guide
-https://cdn.sparkfun.com/datasheets/Sensors/Weather/Si7021.pdf
+* sensor: https://learn.sparkfun.com/tutorials/si7021-humidity-and-temperature-sensor-hookup-guide
 
-https://learn.sparkfun.com/tutorials/photon-development-guide/particle-build-online
-
-use adafruit for logging
+* use adafruit for logging
 https://learn.adafruit.com/home-automation-in-the-cloud-with-the-esp8266-and-adafruit-io/hardware-and-software-requirements
 
-* particle web api docs
+* Rejected ideas: Thingspeak talks to particle, but not worth it because matlab centric
 
-curl "https://api.particle.io/v1/devices/0123456789abcdef/temp?access_token=123412341234"
-
-* how to talk to adafruit
-
-https://www.hackster.io/anasdalintakam/particle-photon-mqtt-integration-with-adafruit-io-6e1d22
-
-* Rejected ideas
-Thingspeak talks to particle, but not worth it because matlab centric
-
-* cli setup
-https://docs.particle.io/tutorials/developer-tools/cli/
+* cli setup: https://docs.particle.io/tutorials/developer-tools/cli/
--- humidity.ino
+++ humidity.ino
@@ -24,9 +24,6 @@ void setup()
 {
     Log.info("in setup");
 
-    // Test function for particle
-    Particle.function("led",ledToggle);
-
     // If our sensor is connected expose our vars
     if(sensor.begin()) {
         Particle.variable("temperature", tempC);
@@ -60,6 +57,11 @@ void loop()
         {
             String asStr(rH);
             Particle.publish("humidity", asStr, PRIVATE);
+
+            // Have a separate threshold for an alarm (ifttt)
+            const float maxHumid = 0.65;
+            if(rH > maxHumid)
+                Particle.publish("alarm-humidity", asStr, PRIVATE);
         }
         {
             String asStr(tempC);
@@ -68,31 +70,4 @@ void loop()
     }
 
     delay(1000);                                                                                     // wait one sec. no need to keep hammering the sensor too often
-}
-
-
-// Sample code test function - delete soon
-int ledToggle(String command) {
-    /* Particle.functions always take a string as an argument and return an integer.
-       Since we can pass a string, it means that we can give the program commands on how the function should be used.
-       In this case, telling the function "on" will turn the LED on and telling it "off" will turn the LED off.
-       Then, the function returns a value to us to let us know what happened.
-       In this case, it will return 1 for the LEDs turning on, 0 for the LEDs turning off,
-       and -1 if we received a totally bogus command that didn't do anything to the LEDs.
-     */
-
-    if (command=="on") {
-        // digitalWrite(led1,HIGH);
-        RGB.control(true);
-        RGB.color(255, 0, 0);                                                                                                                                                                         // red
-        return 1;
-    }
-    else if (command=="off") {
-        RGB.color(0, 255, 0);                                                                                                                                                                         // green
-        RGB.control(false);
-        return 0;
-    }
-    else {
-        return -1;
-    }
 }
